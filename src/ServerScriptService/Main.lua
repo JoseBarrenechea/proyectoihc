b68c228564d4e6c09787245f1a7d4e481325eba2
JoseBarrenechea <102694691+JoseBarrenechea@users.noreply.github.com>
2025-11-07 19:23:04 -0500

    aaaaa

---
+++ src/ServerScriptService/Main.lua
@@ -0,0 +1,29 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Structures = ReplicatedStorage:WaitForChild("Structures")
+local StartEvent = ReplicatedStorage:WaitForChild("START")
+local GameModule = require(script.Game)
+
+local empezo = false 
+
+StartEvent.OnServerEvent:Connect(function()
+	
+	local workers = game.Players:GetPlayers()
+	
+	local gameInstance = GameModule.new(workers)
+
+	gameInstance:start()
+
+	
+end)
+
+ 
+-- Agregar/remover workers dinámicamente
+game.Players.PlayerAdded:Connect(function(player)
+	--gameInstance:addWorker(player)
+end)
+
+game.Players.PlayerRemoving:Connect(function(player)
+	--gameInstance:removeWorker(player)
+end)
+
+ 
